9a1e205db1203e84cc9c95b9671974279cf19bb3
pirogovsky <>
2018-02-14 09:24:04 -0800

    Initial stub projects for client and server

---
+++ ProtoBufClientServer/ProtoBufClientServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProtoBufClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
